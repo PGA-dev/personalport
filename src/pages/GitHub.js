303d63c1a14af414d7770e928aab1c29116845b3
PGA-dev <paulgadam@gmail.com>
2023-11-17 10:00:16 -0700

    added GitHub page skele

---
+++ src/pages/GitHub.js
@@ -0,0 +1,23 @@
+import { Row, Col } from 'reactstrap';
+
+const GitHub = () => {
+
+    return (
+    <div classname='github'>
+        <Row>
+            <Col>
+                Cool GitHub crap
+            </Col>
+        </Row>
+
+
+    </div> 
+    
+    )
+        
+
+
+    
+}
+
+export default GitHub;
