e11313fd626e85e04a2b513df8da68615fe8a3c7
PGA-dev <paulgadam@gmail.com>
2023-11-19 18:55:45 -0700

    updated potential navigation bar

--- src/site/Header.js
+++ src/site/Header.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 import styles from './Header.module.css'
-import { Link } from 'react-router-dom';
 
 const Header = () => {
 
@@ -25,6 +24,7 @@ const Header = () => {
                     <h1>PGA</h1>
                 </animated.div>
             </div>
+
             <div>
                 <animated.div style={animatedStyle}>
                     <h3 className='text-center'><i>Full Stack Developer</i></h3>
@@ -40,21 +40,12 @@ const Header = () => {
                     <h3 className='text-center'><i>Full Stack Developer</i></h3>
                 </animated.div>
             </div>
-
-            <div className={styles.dropdown}>
+            <div>
                 <animated.div style={animatedStyle}>
-                    <span><h1>Links</h1></span>
-                    <div className={styles.dropDownContent}>
-                    <h5 ><Link to='/' className={styles.l}><i>Go Home</i></Link></h5>
-                        <h5 ><Link to='/contact' className={styles.l}><i>Go to Contacts</i></Link></h5>
-                        <h5><Link to='/projects' className={styles.l}><i>Go to Projects</i></Link></h5>
-                        <h5><Link to='/history' className={styles.l}><i>Go to History</i></Link></h5>
-                        <h5><Link to='/resume' className={styles.l}><i>Go to Resume</i></Link></h5>
-                        <h5><Link to='/sitemap' className={styles.l}><i>Go to SiteMap</i></Link></h5>
-
-                    </div>
+                    <h1>PGA</h1>
                 </animated.div>
             </div>
+
         </div>
     );
 };
--- src/site/Nav.js
+++ src/site/Nav.js
@@ -1,15 +1,26 @@
-// import { useState, useEffect} from 'react';
-//import {Col} from 'reactstrap';
-
-// import { NavLink } from 'react-router-dom';
-// import { useSpring, animated } from 'react-spring';
+import { Link } from 'react-router-dom';
 
 
-// const NavHeader = () => {
-//     // const [menuOpen, setMenuOpen] = useState(false);
-//     return (
-// <div></div>
-//     );
-// };
 
-// export default NavHeader;
+const NavHeader = () => {
+
+    return (
+        <div>
+        <div className={styles.dropdown}>
+            <span><h1>Links</h1></span>
+            <div className={styles.dropDownContent}>
+                <h5 ><Link to='/' className={styles.h}><i>Go Home</i></Link></h5>
+                <h5 ><Link to='/contact' className={styles.h}><i>Go to Contacts</i></Link></h5>
+                <h5><Link to='/projects' className={styles.h}><i>Go to Projects</i></Link></h5>
+                <h5><Link to='/history' className={styles.h}><i>Go to History</i></Link></h5>
+                <h5><Link to='/resume' className={styles.h}><i>Go to Resume</i></Link></h5>
+                <h5><Link to='/sitemap' className={styles.h}><i>Go to SiteMap</i></Link></h5>
+            </div>
+
+        </div>
+        
+        </div>
+    );
+};
+
+export default NavHeader;
