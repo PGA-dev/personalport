125b9d58dab7d0e42ae8eec485c1a19d1ee9329a
PGA-dev <paulgadam@gmail.com>
2023-11-22 19:35:55 -0700

    updated links header to include home and site map, adjusting mobile spacing for div containers in nab

--- src/site/Nav.js
+++ src/site/Nav.js
@@ -5,16 +5,18 @@ import styles from './Nav.module.css'
 const Nav = () => {
 
     return (
-        <div>
+        <div className={styles.container}>
             <ul>
-                <li><Link to='/' className={styles.n}><i>Go Home</i></Link></li>
-                <li><Link to='/sitemap' className={styles.n}><i>Go to SiteMap</i></Link></li>
+                <li><Link to='/' className={styles.n}><i>Home</i></Link></li>
+                <li><Link to='/sitemap' className={styles.n}><i>SiteMap</i></Link></li>
                 <li className={`${styles.dropDown} ${styles.dropBtn} ${styles.n}`}><i>Links</i>
                     <div className={styles.dropDownContent}>
-                        <h5 ><Link to='/contact' className={styles.n}><i>Go to Contacts</i></Link></h5>
-                        <h5><Link to='/projects' className={styles.n}><i>Go to Projects</i></Link></h5>
-                        <h5><Link to='/history' className={styles.n}><i>Go to History</i></Link></h5>
-                        <h5><Link to='/resume' className={styles.n}><i>Go to Resume</i></Link></h5>
+                        <h5><Link to='/' className={styles.n}><i>Home</i></Link></h5>
+                        <h5 ><Link to='/contact' className={styles.n}><i>Contacts</i></Link></h5>
+                        <h5><Link to='/projects' className={styles.n}><i>Projects</i></Link></h5>
+                        <h5><Link to='/history' className={styles.n}><i>History</i></Link></h5>
+                        <h5><Link to='/resume' className={styles.n}><i>Resume</i></Link></h5>
+                        <h5><Link to='/sitemap' className={styles.n}><i>SiteMap</i></Link></h5>
                     </div>
                 </li>
             </ul>
