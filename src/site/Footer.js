c46c0b16787eab6aba12917cd7e39ce461481f87
PGA-dev <paulgadam@gmail.com>
2023-11-23 20:22:04 -0700

    working on responsive approaches and cleanup

--- src/site/Footer.js
+++ src/site/Footer.js
@@ -18,59 +18,55 @@ const Footer = () => {
     }, []);
     return (
         <footer className={styles.footer}>
+            <div>
+                <animated.div style={animatedStyle}>
+                    <h1 >Site Links</h1>
+                </animated.div>
+                <h5 ><Link to='/' className={styles.l}><i>Go Home</i></Link></h5>
+                <h5 ><Link to='/contact' className={styles.l}><i>Go to Contacts</i></Link></h5>
+                <h5><Link to='/projects' className={styles.l}><i>Go to Projects</i></Link></h5>
+                <h5><Link to='/history' className={styles.l}><i>Go to History</i></Link></h5>
+                <h5><Link to='/resume' className={styles.l}><i>Go to Resume</i></Link></h5>
+                <h5><Link to='/sitemap' className={styles.l}><i>Go to SiteMap</i></Link></h5>
 
-                <div>
-                    <animated.div style={animatedStyle}>
-                        <h1 >Site Links</h1>
+            </div>
+            <div>
+                <animated.div style={animatedStyle}>
+                    <img src={profile} alt='profile' width="400"
+                        height="auto" className='profile' />
+                </animated.div>
 
-                    </animated.div>
-             
-                   
-                        <h5 ><Link to='/' className={styles.l}><i>Go Home</i></Link></h5>
-                        <h5 ><Link to='/contact' className={styles.l}><i>Go to Contacts</i></Link></h5>
-                        <h5><Link to='/projects' className={styles.l}><i>Go to Projects</i></Link></h5>
-                        <h5><Link to='/history' className={styles.l}><i>Go to History</i></Link></h5>
-                        <h5><Link to='/resume' className={styles.l}><i>Go to Resume</i></Link></h5>
-                        <h5><Link to='/sitemap' className={styles.l}><i>Go to SiteMap</i></Link></h5>
-                    
-                </div>
-                <div>
-                    <animated.div style={animatedStyle}>
-                        <img src={profile} alt='profile' width="auto"
-                            height="auto" className='profile' />
-                    </animated.div>
-
-                    </div>
-                    <div>
-                    <animated.div style={animatedStyle}><h1 >Author Links</h1>
-                    </animated.div>
-                    <animated.div style={animatedStyle}>
-                        <dl>
-                            <dt >
-                                <a className={styles.a} href="https://www.linkedin.com/in/paul-adam-86ba423b/" target="_blank" rel="noreferrer">PGA's Linked In</a>
-                            </dt>
-                            <dt>
-                                <a className={styles.a} href='https://www.youtube.com/channel/UCdSa6weDVHCPFJzjE9M4foA' target="_blank" rel="noreferrer">PGA's YouTube</a>
-                            </dt>
-                            <dt>
-                                <a
-                                    className={styles.a}
-                                    role='button'
-                                    href='mailto:paulgadam@gmail.com'
-                                >
-                                    <i className='' /> PGA Email
-                                </a>
-                            </dt>
-                            <dt>
+            </div>
+            <div>
+                <animated.div style={animatedStyle}><h1 >Author Links</h1>
+                </animated.div>
+                <animated.div style={animatedStyle}>
+                    <dl>
+                        <dt >
+                            <a className={styles.a} href="https://www.linkedin.com/in/paul-adam-86ba423b/" target="_blank" rel="noreferrer">PGA's Linked In</a>
+                        </dt>
+                        <dt>
+                            <a className={styles.a} href='https://www.youtube.com/channel/UCdSa6weDVHCPFJzjE9M4foA' target="_blank" rel="noreferrer">PGA's YouTube</a>
+                        </dt>
+                        <dt>
+                            <a
+                                className={styles.a}
+                                role='button'
+                                href='mailto:paulgadam@gmail.com'
+                            >
+                                <i className='' /> PGA Email
+                            </a>
+                        </dt>
+                        <dt>
                             <a className={styles.a} href='https://github.com/PGA-dev' target="_blank" rel="noreferrer"> PGA's GitHub</a>
-                            </dt>
-                            <dt>
+                        </dt>
+                        <dt>
                             <a className={styles.a} href='https://gitlab.com/pga_dev/Portfolio' target="_blank" rel="noreferrer">PGA's GitLab</a>
-                            </dt>
-                            <br></br>
-                        </dl>
-                    </animated.div>
-                </div>
+                        </dt>
+                        <br></br>
+                    </dl>
+                </animated.div>
+            </div>
 
         </footer>
 
