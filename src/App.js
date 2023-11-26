114962acef4cdaa3d1af188ca2e9ab2c11c01b8d
PGA-dev <paulgadam@gmail.com>
2023-11-26 10:12:16 -0700

    added lazy load to router on app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {Component, lazy, Suspense} from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import { Counter } from './features/counter/Counter';
 import './App.css';
 import Footer from './site/Footer';
-import HomePage from './pages/HomePage';
-import Contact from './pages/Contact';
-import Projects from './pages/Projects';
-import History from './pages/History';
-import Resume from './pages/Resume';
-import SiteMap from './pages/SiteMap';
+// import HomePage from './pages/HomePage';
+// import Contact from './pages/Contact';
+// import Projects from './pages/Projects';
+// import History from './pages/History';
+// import Resume from './pages/Resume';
+// import SiteMap from './pages/SiteMap';
 import Header from './site/Header';
 import Nav from './site/Nav';
+
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+const Contact = lazy(() => import('./pages/Contact'));
+const Projects = lazy(() => import('./pages/Projects'));
+const History = lazy(() => import('./pages/Resume'));
+const Resume = lazy(() => import('./pages/HomePage'));
+const SiteMap = lazy(() => import('./pages/SiteMap'));
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
     <Header /> 
+      <Suspense fallback={<div>Please Wait, Loading...</div>}>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/contact' element={<Contact />} />
@@ -25,7 +35,7 @@ function App() {
         <Route path='/resume' element={<Resume />} />
         <Route path='/sitemap' element={<SiteMap />} />
       </Routes>
-
+    </Suspense>
       <Footer />
     </div>
   );
