1808bc31f829cad2dc793b39378ef46718583b7a
PGA-dev <paulgadam@gmail.com>
2023-11-26 01:13:37 -0700

    added sticky nav

--- src/site/Nav.js
+++ src/site/Nav.js
@@ -3,28 +3,22 @@ import styles from './Nav.module.css'
 import profile from '../app/profile.png';
 
 const Nav = () => {
-
+//possibly make sticky nav without dropdown...too few links to be useful in dropdown form currently
     return (
         <div className={styles.container}>
             <ul>
                 <li><Link to='/' className={styles.n}><i>Home</i></Link></li>
                 <li><Link to='/sitemap' className={styles.n}><i>SiteMap</i></Link></li>
-                <li className={`${styles.dropDown} ${styles.dropBtn} ${styles.n}`}><i>Links</i>
-                    <div className={styles.dropDownContent}>
-                        <h5><Link to='/' className={styles.n}><i>Home</i></Link></h5>
-                        <h5 ><Link to='/contact' className={styles.n}><i>Contacts</i></Link></h5>
-                        <h5><Link to='/projects' className={styles.n}><i>Projects</i></Link></h5>
-                        <h5><Link to='/history' className={styles.n}><i>History</i></Link></h5>
-                        <h5><Link to='/resume' className={styles.n}><i>Resume</i></Link></h5>
-                        <h5><Link to='/sitemap' className={styles.n}><i>SiteMap</i></Link></h5>
-                    </div>
-                    
-                </li>
+                <li><Link to='/contact' className={styles.n}><i>Contacts</i></Link></li>
+                <li><Link to='/projects' className={styles.n}><i>Projects</i></Link></li>
+                <li><Link to='/history' className={styles.n}><i>History</i></Link></li>
+                <li><Link to='/resume' className={styles.n}><i>Resume</i></Link></li>
                 <div className={styles.profile}> <img src={profile} alt='profile' className='profile' height={75} /> </div>
             </ul>
             
         </div>
     );
 };
+
 
 export default Nav;
