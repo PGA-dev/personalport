a9039d61d97a371ad39b63590a5fd45968aff5c6
PGA-dev <paulgadam@gmail.com>
2023-11-26 12:10:19 -0700

    added skills table to resume.js, updated css mod

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -39,7 +39,63 @@ const Resume = () => {
                 </animated.div></div>
 
             <div className={styles.item4}>
-                <h5>Skills table</h5>
+                <h3>Skills</h3>
+
+                <table id='projects'>
+                    <caption>
+                        Skills Proficiency Table
+                    </caption>
+                    <tr>
+                        <th>Skill</th>
+                        <th>Proficiency Level</th>
+                        <th>Years Used</th>
+                        <th>Certificates</th>
+                        <th>Projects/Companies Used</th>
+                        <th>Notes</th>
+                    </tr>
+                    <tr>
+                        <td>Html</td>
+                        <td>Intermediate-Advanced</td>
+                        <td>25</td>
+                        <td>NuCamp Web Fund</td>
+                        <td>Ebay, RDI, Nucamp</td>
+                        <td>No notes</td>
+                    </tr>
+                    <tr>
+                        <td>Javascript</td>
+                        <td>Beginner-Intermediate</td>
+                        <td>4-5</td>
+                        <td>NuCamp React, React Native, Bootstrap</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+
+
+                </table>
             </div>
 
         </div>
