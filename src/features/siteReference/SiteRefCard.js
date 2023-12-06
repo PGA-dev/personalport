308fe17ea5e2f1e07be1af438596fbb2cfddc0da
PGA-dev <paulgadam@gmail.com>
2023-12-06 08:39:46 -0700

    updated css mod for reference card

--- src/features/siteReference/SiteRefCard.js
+++ src/features/siteReference/SiteRefCard.js
@@ -4,14 +4,17 @@ const SiteRefCard = ({ references }) => {
 
     const { id, name, link, linkText, message } = references;
 
+    const cardNum = id +1;
+
     return (
         <div className={styles.card}>
             <dl>
-                <dt>{id}</dt>
-                <dt>{name}</dt>
-                <dt>{message}</dt>
-                <dt><a href={link} target="_blank" rel="noreferrer">
-                    {linkText}</a>
+                <dt><span>Reference Id:</span> {cardNum}</dt>
+                <dt><span>Name: </span>{name}</dt>
+                <dt><span>Reference</span>{message}</dt>
+                <dt><span>Link to Source: 
+                    </span><a href={link} target="_blank" rel="noreferrer">
+                     {linkText}</a>
                 </dt>
             </dl>
 
