1297539d0759d1f8a1c120e11d72fbb6cfdd8945
PGA-dev <paulgadam@gmail.com>
2023-12-17 23:12:55 -0700

    updated store for reducers for home, projects, resume

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import { referencesReducer } from '../features/siteReference/siteReferenceSlice';
+import { homeReducer } from '../features/home/homeSlice';
+import { projectsReducer } from '../features/projects/projectsSlice';
+import { resumeReducer } from '../features/resume/resumeSlice';
 
 export const store = configureStore({
   reducer: {
+    home: homeReducer,
+    projects: projectsReducer,
+    resume: resumeReducer,
     counter: counterReducer,
     references: referencesReducer,
   },
