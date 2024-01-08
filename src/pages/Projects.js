7bd776d2eaa346c0cbf5b22e58e51d8bf1e24876
PGA-dev <paulgadam@gmail.com>
2024-01-07 19:01:02 -0700

    1.7.24 updated links and information on project table

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 import styles from './Projects.module.css';
+import { FaLink } from "react-icons/fa";
 
 const Projects = () => {
 
@@ -44,54 +45,54 @@ const Projects = () => {
 
                     </tr>
                     <tr>
-                        <td data-cell="date">September 2023</td>
+                        <td data-cell="date">September 2023 - Present</td>
                         <td data-cell="name">Portfolio</td>
                         <td data-cell="company">Personal</td>
                         <td data-cell="technology">React,Redux,React Browser,HTML,CSS</td>
                         <td data-cell="repo">
-                            <a className={styles.a} href='https://github.com/PGA-dev/personalport' target="_blank" rel="noreferrer"><i>GH: portfolio</i></a>
+                            <a className={styles.a} href='https://github.com/PGA-dev/personalport' target="_blank" rel="noreferrer"><FaLink/></a>
                         </td>
                         <td data-cell="notes">Static portfolio site</td>
                     </tr>
                     <tr>
-                        <td data-cell="date">August/September 2022</td>
+                        <td data-cell="date">June 2022</td>
                         <td data-cell="name">Match Game</td>
                         <td data-cell="company">NuCamp</td>
                         <td data-cell="technology">HTML, CSS, Javascript</td>
                         <td data-cell="repo">
-                            <a className={styles.a} href='https://github.com/PGA-dev/match_game' target="_blank" rel="noreferrer"><i>GH: match game</i></a>
+                            <a className={styles.a} href='https://github.com/PGA-dev/match_game' target="_blank" rel="noreferrer"><FaLink/></a>
                         </td>
                         <td data-cell="notes">Final Project for Web Dev Fundamentals class, enhanced by PGA</td>
                     </tr>
                     <tr>
-                        <td data-cell="date">August/September 2022</td>
+                        <td data-cell="date">March 2023</td>
                         <td data-cell="name">Guitar Information Guide</td>
                         <td data-cell="company">NuCamp</td>
                         <td data-cell="technology">HTML, CSS, Javascript</td>
                         <td data-cell="repo">
-                            <a className={styles.a} href='https://github.com/PGA-dev/match_game' target="_blank" rel="noreferrer"><i>GH: match game</i></a>
+                            <a className={styles.a} href='https://github.com/PGA-dev/pgaguitar_pp' target="_blank" rel="noreferrer"><FaLink/></a>
                         </td>
-                        <td data-cell="notes">Final Project for Web Dev Fundamentals class, enhanced by PGA</td>
+                        <td data-cell="notes">Final Project for React.js, and Front end honors graduation</td>
                     </tr>
                     <tr>
-                        <td data-cell="date">August/September 2022</td>
+                        <td data-cell="date">September 2022</td>
                         <td data-cell="name">Grocery App</td>
                         <td data-cell="company">NuCamp</td>
                         <td data-cell="technology">HTML, CSS, Javascript</td>
                         <td data-cell="repo">
-                            <a className={styles.a} href='https://github.com/PGA-dev/match_game' target="_blank" rel="noreferrer"><i>GH: match game</i></a>
+                            <a className={styles.a} href='https://github.com/PGA-dev/grocery' target="_blank" rel="noreferrer"><FaLink/></a>
                         </td>
-                        <td data-cell="notes">Final Project for Web Dev Fundamentals class, enhanced by PGA</td>
+                        <td data-cell="notes">Final Project for Python/Dev Ops/SQL classes, and Back End honors graduation</td>
                     </tr>
                     <tr>
-                        <td data-cell="date">August/September 2022</td>
+                        <td data-cell="date">April 2023</td>
                         <td data-cell="name">Full Stack Guitar Site</td>
                         <td data-cell="company">NuCamp</td>
-                        <td data-cell="technology">HTML, CSS, Javascript</td>
+                        <td data-cell="technology">HTML, CSS, Javascript, Node.Js, React, Redux</td>
                         <td data-cell="repo">
-                            <a className={styles.a} href='https://github.com/PGA-dev/match_game' target="_blank" rel="noreferrer"><i>GH: match game</i></a>
+                            <a className={styles.a} href='https://github.com/PGA-dev/pgaguitarexp' target="_blank" rel="noreferrer"><FaLink/></a>
                         </td>
-                        <td data-cell="notes">Final Project for Web Dev Fundamentals class, enhanced by PGA</td>
+                        <td data-cell="notes">Final Project for Node JS class, and honors graduation project</td>
                     </tr>
                 </table>
             </div>
