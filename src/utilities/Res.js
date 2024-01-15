91232574b90ccb603c9857e96e0daffac4d29b96
PGA-dev <paulgadam@gmail.com>
2024-01-15 15:03:07 -0700

    1.15.24 added resume.docx

--- src/utilities/Res.js
+++ src/utilities/Res.js
@@ -1,11 +1,20 @@
 // import resume from '../app/resume.docx';
 import styles from "./Res.module.css"
-
+import DocViewer, {DocViewerRenderers} from "@cyntler/react-doc-viewer";
+//uri: require("../app/resources/FullStackDev.docx")
 const Res = () => {
+
+    const resdocs = [
+        {uri: "https://github.com/PGA-dev/document/raw/main/word/FullStack.docx",
+        fileType: "docx",
+        fileName: "FullStackDev.docx"
+        }
+    ]
 
     return (
         <>
-        <h4 className={styles.res}>Future resume download Here...</h4>
+        <DocViewer className={styles.res} documents={resdocs} pluginRenderers={DocViewerRenderers}
+        />
         </>
         
     )
