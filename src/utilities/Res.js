298db3524ede729a94d49447a46c2f3896102bcb
PGA-dev <paulgadam@gmail.com>
2024-01-20 17:43:46 -0700

    1.20.24 added margin to docx file renderer

--- src/utilities/Res.js
+++ src/utilities/Res.js
@@ -1,7 +1,8 @@
 // import resume from '../app/resume.docx';
-// import styles from "./Res.module.css"
+import styles from "./Res.module.css"
 import DocViewer, {DocViewerRenderers} from "@cyntler/react-doc-viewer";
 //uri: require("../app/resources/FullStackDev.docx") -- current version of docx renderers don't work well with localhost uri
+//
 const Res = () => {
 
     const resdocs = [
@@ -15,7 +16,7 @@ const Res = () => {
         <>
         <DocViewer documents={resdocs} 
         pluginRenderers={DocViewerRenderers}
-        style={{height: 1250}}
+        style={{height:1000, width:1550, margin:50}}
         />
         </>
         
